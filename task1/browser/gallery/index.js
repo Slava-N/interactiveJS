ed19a99022fec336495e08e9a6dab98d6b8bef1a
slava-n <novozhilovu@gmail.com>
2019-01-21 00:15:46 +0300

    task 1 amended

--- task1/browser/gallery/index.js
+++ task1/browser/gallery/index.js
@@ -6,20 +6,25 @@ const imgNames = ['i/breuer-building.jpg','i/guggenheim-museum.jpg','i/headquart
 
 const currentPhoto = document.getElementById('currentPhoto');
 let imgCounter = 0;
+currentPhoto.src = imgNames[imgCounter];
 
 const prevPhoto = document.getElementById('prevPhoto');
 const nextPhoto = document.getElementById('nextPhoto')
 
+
+
 function slideRight() {
-    
+
+    imgCounter === (imgNames.length - 1 - 1) ? imgCounter = 0 : imgCounter++;
     currentPhoto.src = imgNames[imgCounter];
-    imgCounter === 5 ? imgCounter = 0 : imgCounter++;
+
 }
 
 
 function slideLeft() {
+    imgCounter === 0 ? imgCounter = (imgNames.length - 1 - 1) : imgCounter--;
     currentPhoto.src = imgNames[imgCounter];
-    imgCounter === 0 ? imgCounter = 5 : imgCounter--;
+    
 }
 
 
--- task1/browser/slider/index.js
+++ task1/browser/slider/index.js
@@ -8,10 +8,12 @@ let imgCounter = 0;
 
 const currentFocus = document.getElementById('slider')
 
+currentFocus.src = imgBase[imgCounter];
+
 function slide() {
     currentFocus.src = imgBase[imgCounter];
-    imgCounter === 5 ? imgCounter = 0 : imgCounter++;
+    imgCounter === ( imgBase.length - 1 - 1 ) ? imgCounter = 0 : imgCounter++;
 
 }
 
-setInterval(slide, 5000)
+setInterval(slide, 5000);
