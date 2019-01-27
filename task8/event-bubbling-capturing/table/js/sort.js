90ac310c23d6898c4add34d975b932b8096866ab
slava-n <novozhilovu@gmail.com>
2019-01-28 00:42:34 +0300

    correction in sort

--- task8/event-bubbling-capturing/table/js/sort.js
+++ task8/event-bubbling-capturing/table/js/sort.js
@@ -4,21 +4,20 @@
 const table = document.querySelector('table');
 table.addEventListener('click', handleTableClick);
 
+
+
 function handleTableClick(event) {
     let targetSpace = event.target;
     if (targetSpace.classList.contains('prop__name')) {
         table.dataset.sortBy = targetSpace.dataset.propName;
         targetSpace.dataset.dir = changeDir(targetSpace.dataset.dir);
-        sortTable(targetSpace.dataset.propName, table.dataset.dir)
-        console.log(event.target);
+        console.log(targetSpace.dataset.propName, )
+        sortTable(targetSpace.dataset.propName, targetSpace.dataset.dir);
     }
 }
 
 function changeDir(dir) {
-    return (dir === '1' ? -1 : 1);
+    return (dir === "1" ? "-1" : "1");
 }
 
-
-// console.log(changeDir('1'))
-// console.log(changeDir('-1'))
 
