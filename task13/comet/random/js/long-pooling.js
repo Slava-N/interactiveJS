daab1806ca4ad5b15fcadf217b6c575305321fc1
slava-n <novozhilovu@gmail.com>
2019-02-11 00:04:31 +0300

    task added

--- task13/comet/random/js/long-pooling.js
+++ task13/comet/random/js/long-pooling.js
@@ -1,1 +1,17 @@
 'use strict';
+
+
+function longPooling() {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'https://neto-api.herokuapp.com/comet/long-pooling');
+    request.addEventListener('load', update);
+    request.send();
+}
+
+function update(event) {
+    highlightNumber('long-pooling', event.target.responseText);
+    console.log('long',event.target.responseText)
+    longPooling();
+}
+
+longPooling();
